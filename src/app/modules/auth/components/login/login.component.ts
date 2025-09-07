0d830f34a610e3374c665c481367ebe7e55e6764
cometa2000 <tuxmorro2016@gmail.com>
2025-09-07 04:56:51 -0600

    final de roles y permisos

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -14,8 +14,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class LoginComponent implements OnInit, OnDestroy {
   // KeenThemes mock, change it to:
   defaultAuth: any = {
-    email: 'brayansolis@gmail.com',
-    password: 'cometa50',
+    email: 'super_admin_crm@gmail.com',
+    password: '12345678',
   };
   loginForm: FormGroup;
   hasError: boolean;
